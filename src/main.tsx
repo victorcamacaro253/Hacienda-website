b7146b62ba3941a24ac5aaccc3fd9616bf0b2c01
victor <victorcamacaro253@gmail.com>
2025-07-22 22:44:59 -0400

    updates 22th jul

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-<script src="https://cdn.counter.dev/script.js" data-id="8f2046aa-2d54-468c-bf30-9edcd5615f89" data-utcoffset="-4"></script>
 
 
 createRoot(document.getElementById('root')!).render(
